66959b2e7ed5cc94ffe175f1c430409da060eb23
Luifer1994 <almendralesluifer@gmail.com>
2025-12-23 10:24:51 -0500

    ok

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -14,7 +14,7 @@ public class HelloController {
     @GetMapping("/hello")
     public Map<String, String> hello() {
         Map<String, String> response = new HashMap<>();
-        response.put("message", "¡Hola desde Spring Boot con Java 25 Melo!");
+        response.put("message", "¡Hola munndo desde Java + Spring Boot!");
         return response;
     }
 }
